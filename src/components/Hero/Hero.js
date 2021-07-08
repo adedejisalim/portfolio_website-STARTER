8f20f9318b88f103537895d6abc3659637ac446c
salim <s534027@nwmissouri.edu>
2021-07-08 16:40:27 -0500

    completed projects section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Salim Adedeji <br />
+        Personal Portfolio
+      </SectionTitle>
+
+      <SectionText>
+      An undergraduate student in Northwest Missouri State University. Pursuing a Bachelor of science degree in computer science. Dependable, hard worker ready to learn and provide adequate service in both Frontend and Backend projects.
+      </SectionText>
+
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
